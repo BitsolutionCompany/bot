7c86e2ddd43573a43fceb9f39101cfc25ab02a67
Bitsolution Company <silvawesley978@gmail.com>
2024-09-20 16:49:11 -0300

    Slider

--- src/components/Carrousel.components.js
+++ src/components/Carrousel.components.js
@@ -1,14 +1,38 @@
 import React from "react"
-// import Slider from "react-slick"
+import Slider from "react-slick"
 import './style/Carroussel.modules.css'
 
 
 function CARROUSEL(){
+    var settings = {
+      dots: false,
+      infinite: true,
+      speed: 2000,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      cssEase: "linear"
+    };
     return(
-      <div className="caixa">
-        <img src={require('./../images/carrousel/item1.png')} alt="nome da imagem" />
-        {/* <img src={require('./../images/carrousel/item2.png')} alt="nome da imagem" /> */}
-      </div>
+      <>
+        {/* Link que faz o slik funcionar */}
+        <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+        {/* items do slider */}
+        <div className="caixa">
+          <div className="slider-container">
+            <Slider {...settings}>
+              <div>
+                <img src={require('./../images/carrousel/item1.png')} alt="nome da imagem" />
+              </div>
+              <div>
+                <img src={require('./../images/carrousel/item2.png')} alt="nome da imagem" />
+              </div>
+            </Slider>
+          </div>
+        </div>
+      </>
     )
 }
 
