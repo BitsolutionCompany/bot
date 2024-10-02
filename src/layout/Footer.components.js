b14fede239ca1686949afb8bfa1a19c99777dbe7
Bitsolution Company <silvawesley978@gmail.com>
2024-10-02 10:06:19 -0300

    'Footer'

--- src/layout/Footer.components.js
+++ src/layout/Footer.components.js
@@ -1,11 +1,68 @@
-import './style/Footer.modules.css'
+import './style/Footer.modules.css';
+import { useState, useEffect } from 'react';
 
-function FOOTER(){
-    return(
-        <footer className="footer">
-            <h1>FOOTER</h1>
-        </footer>
-    )
+function FOOTER() {
+  const [data, setData] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setData(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <footer class="footer">
+      <div class="data">
+        de 17/02/2024 até {data.toLocaleDateString()}
+      </div>
+      <ul>
+        <li>
+          <h4>São Benedito</h4>
+          <ul>
+            <li>
+              <a href="https://www.instagram.com/toutisaobenedito?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" rel="noopener noreferrer">
+              Instagram
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h4>Guaraciaba do Norte</h4>
+          <ul>
+            <li>
+              <a href="https://www.instagram.com/toutiguaraciabadonorte?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" rel="noopener noreferrer">
+                Instagram
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h4>IPU</h4>
+          <ul>
+            <li>
+              <a href="https://www.instagram.com/toutiipu?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" rel="noopener noreferrer">
+                Instagram
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h4>Ipueiras</h4>
+          <ul>
+            <li>
+              <a href="https://www.instagram.com/ipueirastouti?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" rel="noopener noreferrer">
+                Instagram
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </footer>
+  );
 }
 
 export default FOOTER;
